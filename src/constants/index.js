2fa77568963b733f926132a3e1938b7006342740
Akash Kumar <akashchittaur@gmail.com>
2025-10-15 01:26:57 +0530

    update portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "intern Full Stack Developer",
+    company: "Future Finder.",
+    description: `lorem augue vel justo tincidunt efficitur. Quisque tincidunt elit non tempor porttitor. Nullam vehicula accumsan nunc, accumsan aliquam diam sagittis at. Nam volutpat orci sit amet varius dignissim. Cras ac interdum mauris, eget posuere erat. Phasellus in interdum justo, sit amet viverra ligula. In sed metus risus.`,
+    technologies: ["Javascript", "React.js", "CSS", "Tailwind"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adxbx",
+    description: `lorem augue vel justo tincidunt efficitur. Quisque tincidunt elit non tempor porttitor. Nullam vehicula accumsan nunc, accumsan aliquam diam sagittis at. Nam volutpat orci sit amet varius dignissim. Cras ac interdum mauris, eget posuere erat. Phasellus in interdum justo, sit amet viverra ligula. In sed metus risus.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Fxxebxxk",
+    description: `lorem augue vel justo tincidunt efficitur. Quisque tincidunt elit non tempor porttitor. Nullam vehicula accumsan nunc, accumsan aliquam diam sagittis at. Nam volutpat orci sit amet varius dignissim. Cras ac interdum mauris, eget posuere erat. Phasellus in interdum justo, sit amet viverra ligula. In sed metus risus.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Summer Intern-Frontend Developer",
+    company: "Futer Finder",
+    description: `Developed and maintained responsive web applications using React.js and Tailwind CSS for frontend development,Implemented responsive design principles using Tailwind, improving mobile usability and UI consistency,Gained hands-on experience in full-stack application development and deployment workflows.`,
+    technologies: ["React", "JavaScript", "Tailwind", "CSS"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Gemini clone",
+    image: project1,
+    description:
+      "A fully functional Gemini clone features like generating text and images powered by the Google Gemini API, using React and CSS.",
+    technologies: ["React", "CSS", "JavaScript", "API"],
+  },
+  {
+    title: "Myntra Clone",
+    image: project2,
+    description:
+      "A fully functional Myntra Clone website with features like shopping cart, Intractive and Responsibe User Interface.",
+    technologies: ["HTML", "CSS", "React", "NodeJs"],
+  },
+  {
+    title: "Weather Clone",
+    image: project3,
+    description:
+      "web application that provides real-time weather updates based on user location or search input. Integrated third-party APIs.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "chat App",
+    image: project4,
+    description:
+      "real-time chat application built to enable seamless communication between users with instant message delivery and live user updates.",
+    technologies: ["react", "mongodb", "socket.io", "Express", "redux"],
+  },
+];
+
+export const CONTACT = {
+  address: " Dera Bassi, Mohali , Punjab , 147301 ",
+  phoneNo: "+91 8540027935 ",
+  email: "akashchittaur@gmail.com",
+};
